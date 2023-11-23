6dea02cd3d1f0948d46c034ddfa394e2587786ef
AlexUzumaki <jgarcia289@alumnes.politecnicllevant.cat>
2023-11-23 17:22:06 +0100

    .

---
+++ volums/web/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>PROJECTE VIDEOJOCS</title>
+</head>
+<body>
+    <h1>PROJECTE VIDEOJOCS</h1>
+<nav>
+        <ul>
+            <li><a href="funcion1.php">Mostra de Videojocs</a></li>
+            <li><a href="#assignar">Assignar Codi</a></li>
+            <li><a href="#eliminar">Eliminar Videojocs</a></li>
+            <li><a href="#afegir">Afegir Data Expiració</a></li>
+            <li><a href="#comprovar">Comprovar Repetits</a></li>
+            <li><a href="#comprovarAmpliada">Comprovar Repetits Ampliada</a></li>
+            <li><a href="#eliminarRepetits">Eliminar Repetits</a></li>
+            <li><a href="#modernAntic">Videojoc més Modern i més Antic</a></li>
+            <li><a href="#ordenar">Ordenació Alfabètica de Videojocs</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
