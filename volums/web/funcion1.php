ce51393a55db0d3f0ff088599ccf6f3e79b07086
AlexUzumaki <jgarcia289@alumnes.politecnicllevant.cat>
2023-11-30 15:15:41 +0100

    .

--- volums/web/funcion1.php
+++ volums/web/funcion1.php
@@ -1,6 +1,37 @@
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>PROJECTE VIDEOJOCS</title>
+</head>
+<body>
+    <header>
+        <h1 style="font-family: 'Courier New', monospace;">PROJECTE VIDEOJOCS</h1>
+    </header>
+
+    <nav>
+        <input type="checkbox" id="menu-toggle">
+        <label for="menu-toggle">&#9776; Menú</label>
+        <ul>
+            <li><a href="funcion1.php">TOTS ELS NOSTRES JOCS</a></li>
+            <li><a href="funcion2.php">ID'S DELS JOCS</a></li>
+            <li><a href="funcion3.php">Eliminar Videojocs</a></li>
+            <li><a href="funcion4.php">Afegir Data Expiració</a></li>
+            <li><a href="funcion5.php">Comprovar Repetits</a></li>
+            <li><a href="#comprovarAmpliada">Comprovar Repetits Ampliada</a></li>
+            <li><a href="#eliminarRepetits">Eliminar Repetits</a></li>
+            <li><a href="#modernAntic">Videojoc més Modern i més Antic</a></li>
+            <li><a href="#ordenar">Ordenació Alfabètica de Videojocs</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
 <?php
 include 'funcions.php';
 
 $informacio = carregarInformacio();
 mostrarVideojocs($informacio);
 ?>
+
